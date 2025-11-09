99bcda3fb260822a830bf2cdc84d0e9acd9a8c7f
jongwan <jongbell4@gmail.com>
2025-11-09 23:03:42 +0900

    first init

---
+++ board-common/src/main/java/com/tutomato/boardcommon/BoardCommonApplication.java
@@ -0,0 +1,13 @@
+package com.tutomato.boardcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardCommonApplication.class, args);
+    }
+
+}
---
+++ board-common/src/test/java/com/tutomato/boardcommon/BoardCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomato.boardcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
