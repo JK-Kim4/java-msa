4f5a9a5f1b9e4545bd8bb04070fda927798ef4c5
jongwan <jongbell4@gmail.com>
2025-12-03 17:52:45 +0900

    feat: Coupon Service 모듈 추가

---
+++ coupon-service/src/test/java/com/tutomato/couponservice/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomato.couponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
